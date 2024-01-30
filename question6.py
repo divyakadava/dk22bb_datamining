78f98b7d2745d94d91dcc7aebf2e0642b71e00b0
Divya Kadava <divyakadava990@gmail.com>
2024-01-29 23:07:35 -0500

    question 6 modified
    modified question6

--- question6.py
+++ question6.py
@@ -1,22 +1,22 @@
 def question6_1():
     
     answer = {
-        'equal_width': ['Change', 0], 
-        'equal_frequency': ['Change',0 ]
+        'equal_width': ['No Change', 9], 
+        'equal_frequency': ['No Change',9]
     }
     return answer
 
 def question6_2():
     answer = {
         'equal_width': ['Change', 2], # doubt with 5th point
-        'equal_frequency': ['Change',2 ]
+        'equal_frequency': ['No Change',9]
     }
     return answer
 
 def question6_3():
     answer = {
         'equal_width': ['Change', 2], # doubt with 5th point
-        'equal_frequency': ['Change',2 ]
+        'equal_frequency': ['Change',4 ]
     }
     return answer
 
