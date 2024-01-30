7ea39cc0d75df2c51b2d309be66102926a91a808
Divya Kadava <divyakadava990@gmail.com>
2024-01-29 21:50:06 -0500

    modified questions3, 4, 5, 6, 7, 8, 9, 10
    modified questions with answers

--- question10.py
+++ question10.py
@@ -1,32 +1,39 @@
 def question10_1():
-    """
-    answer: python bool (True or False)
-    """
+    answer=True #a
     return answer
 
 def question10_2():
+    answer=True #b
     return answer
 
 def question10_3():
+    answer=True #c #check out this
     return answer
 
 def question10_4():
+    answer=False #d
     return answer
 
 def question10_5():
+    answer=True #e
     return answer
 
 def question10_6():
-    return answer
+    answer=False  # check out this
+    return answer #f
 
 def question10_7():
-    return answer
+    answer=True
+    return answer #g
 
 def question10_8():
-    return answer
+    answer=True
+    return answer   #h
 
 def question10_9():
-    return answer
+    answer=False  # check out this
+    return answer   #i
 
 def question10_10():
-    return answer
+    answer=False
+    return answer  #j
